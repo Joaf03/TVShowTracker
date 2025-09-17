44d88eee75b4b5913047bccb8e3efc122ae3209c
João Afonso Santos <afonsooliveirasantos2003@gmail.com>
2025-09-17 18:57:14 +0100

    Add environment variable validation

---
+++ backend/src/config/env.ts
@@ -0,0 +1,34 @@
+import { config } from "dotenv"
+
+// Load the environment variables
+config();
+
+const required_env_vars = [
+    "POSTGRES_USER",
+    "POSTGRES_PASSWORD", 
+    "POSTGRES_DB",
+    "POSTGRES_HOST",
+    "POSTGRES_PORT",
+    "DATABASE_URL",
+    "PGADMIN_DEFAULT_EMAIL",
+    "PGADMIN_DEFAULT_PASSWORD"
+] as const;
+
+for (const env_var of required_env_vars) {
+    if (!process.env[env_var]) {
+        throw Error(`Missing environment variable ${env_var}`);
+    }
+};
+
+const env_vars = {
+    DB: {
+        USER: process.env.POSTGRES_USER!,
+        PASSWORD: process.env.POSTGRES_PASSWORD!,
+        DATABASE: process.env.POSTGRES_DB!,
+        HOST: process.env.POSTGRES_HOST!,
+        PORT: parseInt(process.env.POSTGRES_PORT!),
+        URL: process.env.POSTGRES_URL!,
+    },
+} as const;
+
+export default env_vars;
